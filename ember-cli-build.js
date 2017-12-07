1280c52a05907207c014b5405aecf0d15393fd91
Conor O'Dwyer <c9dw5er@protonmail.com>
2017-12-07 23:41:08 +0000

    FontAwesome icons added all versions - Replaced close icon - Replaced Hanburger icon - Replaced play video icon home page - All tests pass

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -22,6 +22,7 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   app.import('vendor/pinterest/sdk.js');
+  app.import('vendor/fontawesome/fontawesome-all.js');
 
   return app.toTree();
 };
